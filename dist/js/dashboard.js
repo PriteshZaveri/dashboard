38c833b2c21bc39ceb13cc6e35666dcf216be817
Zaveri <pritesh.zaveri@accenture.com>
2017-06-01 20:48:06 +0530

    new

---
+++ dist/js/dashboard.js
@@ -0,0 +1,25 @@
+function addResource() {
+  var resDrop = document.getElementById('resValues');
+  var res = resDrop.options[resDrop.selectedIndex].value;
+  var shiftDrop = document.getElementById('shiftVal');
+  var sft = shiftDrop.options[shiftDrop.selectedIndex].value;
+
+
+  if (sft=="A") {
+    var shiftTable = document.getElementById('aShiftData');
+    shiftTable.innerHTML = shiftTable.innerHTML + "<p>"+res+"</p>"
+  }
+  else if (sft=="General") {
+    var shiftTable = document.getElementById('gShiftData');
+    shiftTable.innerHTML = shiftTable.innerHTML + "<p>"+res+"</p>"
+  }
+  else if (sft=="B") {
+    var shiftTable = document.getElementById('bShiftData');
+    shiftTable.innerHTML = shiftTable.innerHTML + "<p>"+res+"</p>"
+  }
+  else if (sft=="C") {
+    var shiftTable = document.getElementById('cShiftData');
+    shiftTable.innerHTML = shiftTable.innerHTML + "<p class="++">"+res+"</p>"
+  }
+
+}
